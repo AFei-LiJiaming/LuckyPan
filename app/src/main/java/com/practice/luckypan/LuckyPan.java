58f25d0fa3bbcacbe337327c6296d12c6ff1a40a
AFei <1012656493@qq.com>
2021-04-17 15:18:24 +0800

    Improve the lottery function

--- app/src/main/java/com/practice/luckypan/LuckyPan.java
+++ app/src/main/java/com/practice/luckypan/LuckyPan.java
@@ -1,5 +1,8 @@
 package com.practice.luckypan;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -7,11 +10,13 @@ import android.graphics.Paint;
 import android.graphics.RectF;
 import android.media.AudioAttributes;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
 import androidx.annotation.Nullable;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class LuckyPan extends View {
 
@@ -19,7 +24,14 @@ public class LuckyPan extends View {
     private ArrayList<RectF> reactFs; //存储矩形集合
     private float strokWidth = 7; //矩形边框宽度
     private int rectSize; //正方形矩形边长
-    private int[] itemColor = {Color.GREEN, Color.YELLOW};
+    private int[] itemColor = {Color.GREEN, Color.RED};
+
+    private int repeatCount = 3; //转圈数
+    private int luckNum ;  //中奖位置
+    private int position = -1;  //抽奖块的位置
+    private int startPosition = 0;  //开始抽奖的位置
+    private boolean clickGoFlag = false;  //是否点击中间Go
+
 
     public LuckyPan(Context context){
         this(context, null);
@@ -88,6 +100,7 @@ public class LuckyPan extends View {
     protected void onDraw(Canvas canvas){
         super.onDraw(canvas);
         drawRects(canvas);
+        drawText(canvas);
     }
 
 
@@ -103,11 +116,86 @@ public class LuckyPan extends View {
                 canvas.drawRect(rectF, paint);
             }
             else {
-                paint.setColor(itemColor[i % 2]);  //保证相邻两矩形颜色不同
+                paint.setColor(itemColor[i % 2]);//保证相邻两矩形颜色不同
+                if (position == i){
+                    paint.setColor(Color.BLUE);
+                }
                 canvas.drawRect(rectF, paint);
             }
         }
     }
 
+    /**
+     * 给矩形添加文字信息
+     */
+    private void drawText(Canvas canvas){
+        for (int i = 0; i < reactFs.size(); i++){
+            RectF rectF = reactFs.get(i);
+            if (i == 8){
+                paint.setTextSize(150);
+                paint.setColor(Color.RED);
+                canvas.drawText("Go", rectF.centerX()-80, rectF.centerY()+50, paint);
+            }
+            else {
+                paint.setTextSize(100);
+                canvas.drawText("" + (i+1), rectF.centerX(), rectF.centerY(), paint);
+            }
+
+        }
+    }
+
+    private void setPosition(int position){
+        this.position = position;
+        invalidate();  //调用刷新
+    }
+
+    /**
+     * 抽奖动画
+     */
+
+    public void startAnimation(){
+        Random random = new Random();
+        luckNum = random.nextInt(8);
+        ValueAnimator valueAnimator = ValueAnimator.ofInt(startPosition, repeatCount * 8 + luckNum).setDuration(6000);
+        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                int position = (int) valueAnimator.getAnimatedValue();
+                setPosition(position % 8);
+            }
+        });
+        valueAnimator.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animator){
+                startPosition = luckNum;
+            }
+        });
+        valueAnimator.start();
+    }
+
+    /**
+     *  重写Go的事件分发
+     */
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN){
+            if (reactFs.get(8).contains(event.getX(), event.getY())){
+                clickGoFlag = true;
+            }
+            else {
+                clickGoFlag = false;
+            }
+            return true;
+        }
+        if (event.getAction() == MotionEvent.ACTION_UP){
+            if (clickGoFlag){
+                if (reactFs.get(8).contains(event.getX(), event.getY())){
+                    startAnimation(); //只有手指落下和抬起都在Go的矩形内才触发
+                }
+                clickGoFlag = false;
+            }
+        }
+        return super.onTouchEvent(event);   //请求抛给父类（事件分发机制）
+    }
 
 }
--- app/src/main/java/com/practice/luckypan/MainActivity.java
+++ app/src/main/java/com/practice/luckypan/MainActivity.java
@@ -3,12 +3,16 @@ package com.practice.luckypan;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private LuckyPan luckyPan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        luckyPan = (LuckyPan) findViewById(R.id.luckPan);
     }
 }
